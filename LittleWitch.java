58a456606aaff6194684b475bb1937ff574177f7
mfadaei <81688309+mehdifadae@users.noreply.github.com>
2023-02-22 15:28:20 +0100

    change1

--- LittleWitch.java
+++ LittleWitch.java
@@ -1,34 +1,41 @@
-package AufgabenFunfteTag;
+package ComCaveHausAufgaben;
 
 public class LittleWitch {
 
     public static void main(String[] args) {
 
         int uhr = Integer.parseInt(args[0]);
+        String text = "Morgens Früh um";
 
-        switch (uhr) {
+        while (uhr <= 12) {
 
-            case 6:
-                System.out.println("Morgens Früh um " + uhr + "\n" + "kommt die kleine Hex.");
-                break;
-            case 7:
-                System.out.println("Morgens früh um " + uhr + "\n" + "schabt sie gelbe Rüben.");
-                break;
-            case 8:
-                System.out.println("Morgens früh um " + uhr + "\n" + "wird Kaffee gemacht.");
-                break;
-            case 9:
-                System.out.println("Morgens früh um " + uhr + "\n" + "geht sie in die Scheun'.");
-                break;
-            case 10:
-                System.out.println("Morgens früh um " + uhr + "\n" + "hack sie Holz und Spän'.");
-                break;
-            case 11:
-                System.out.println("Feuert an um " + uhr + "," + "\n" + "Koch dann bis um 12:");
-                break;
-            default:
-                System.out.println("Fröschebein und Krebs und Fish." + "\n" + "Hurtig Kinder Kommt zu Tisch!");
+            if (uhr <= 10) {
+                System.out.println(text + uhr);
+            }
+            switch (uhr) {
+                case 6:
+                    System.out.println("kommt die kleine Hex.");
+                    break;
+                case 7:
+                    System.out.println("schabt sie gelbe Rüben.");
+                    break;
+                case 8:
+                    System.out.println("wird Kaffee gemacht.");
+                    break;
+                case 9:
+                    System.out.println("geht sie in die Scheun'.");
+                    break;
+                case 10:
+                    System.out.println("hack sie Holz und Spän'.");
+                    break;
+                case 11:
+                    System.out.println("Feuert an um " + uhr + "," + "\n" + "Koch dann bis um 12:");
+                    break;
+                case 12:
+                    System.out.println("Fröschebein und Krebs und Fish." + "\n" + "Hurtig Kinder Kommt zu Tisch!");
+                    break;
+            }
+            uhr++;
         }
     }
-
 }
